b65491556052e1332532d66f67285c029785d371
Juan Sebastian Martinez <sebastianmartinez778@gmail.com>
2025-02-18 21:57:44 -0500

    translate

--- src/app/presentation/pages/translatePage/translatePage.component.ts
+++ src/app/presentation/pages/translatePage/translatePage.component.ts
@@ -1,9 +1,58 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent, TextMessageBoxEvent, TextMessageBoxSelectComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-translate-page',
-  imports: [],
+  imports: [
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxSelectComponent,
+  ],
   templateUrl: './translatePage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TranslatePageComponent { }
+export default class TranslatePageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+  public languages = signal([
+    { id: 'alemán', text: 'Alemán' },
+    { id: 'árabe', text: 'Árabe' },
+    { id: 'bengalí', text: 'Bengalí' },
+    { id: 'francés', text: 'Francés' },
+    { id: 'hindi', text: 'Hindi' },
+    { id: 'inglés', text: 'Inglés' },
+    { id: 'japonés', text: 'Japonés' },
+    { id: 'mandarín', text: 'Mandarín' },
+    { id: 'portugués', text: 'Portugués' },
+    { id: 'ruso', text: 'Ruso' },
+  ]);
+
+  handleMessageWithSelect({ prompt, selectedOption}: TextMessageBoxEvent) {
+
+    const message = `Traduce a ${ selectedOption }: ${ prompt }`;
+    this.isLoading.set(true);
+    this.messages.update!( (prev) => [
+      ...prev,
+      {
+        isGpt: false,
+        text: message,
+      }
+    ]);
+    this.openAiService.translateText( prompt, selectedOption )
+      .subscribe( ({ message }) => {
+        this.isLoading.set(false);
+        this.messages.update( prev => [
+          ...prev,
+          {
+            isGpt: true,
+            text: message,
+          }
+        ])
+      })
+  }
+}
--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { orthographyUseCase, prosConsStreamUseCase, prosConsUseCase } from '@use-cases//index';
+import { orthographyUseCase, prosConsStreamUseCase, prosConsUseCase, translateUseCase } from '@use-cases//index';
 import { from } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
@@ -13,5 +13,9 @@ export class OpenAiService {
   }
   prosConsStreamDiscusser( prompt: string, abortSignal: AbortSignal ) {
     return prosConsStreamUseCase( prompt, abortSignal );
+  }
+
+  translateText( prompt: string, lang: string ) {
+    return from(translateUseCase( prompt, lang ));
   }
 }
